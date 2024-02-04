8fe25016d752dbc5f8c45877bfc15e286d8d1ec1
asadZaidi <asadzaidi201999@gmail.com>
2024-02-04 17:51:56 +0500

    Adding column to 'Table Sitting' table and built a relation with 'Order' table

--- Backend Services/src/main/java/com/restaurant/backend/model/TableSitting.java
+++ Backend Services/src/main/java/com/restaurant/backend/model/TableSitting.java
@@ -3,6 +3,9 @@ package com.restaurant.backend.model;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 @Entity
 @Table(name = "table_sitting")
@@ -10,9 +13,14 @@ public class TableSitting extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-
+    @Column(name = "table_code")
+    private int tableCode;
+    @Column(name = "reserved")
+    private boolean isReserved;
     @ManyToOne
     @JoinColumn(name = "restaurant_id")
     private Restaurant restaurant;
+    @OneToMany(mappedBy = "tableSitting")
+    private List<Order> orders = new ArrayList<>();
 
 }

ebf92be5618b546e371c8fdd94b644cf22300347
asadZaidi <asadzaidi201999@gmail.com>
2024-03-05 00:33:55 +0500

    Adding TableSitting and User DTOs

---
+++ Backend Services/src/main/java/com/restaurant/backend/payloads/TableSittingAdminDTO.java
@@ -0,0 +1,21 @@
+package com.restaurant.backend.payloads;
+
+import com.restaurant.backend.model.Order;
+import com.restaurant.backend.model.Restaurant;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class TableSittingAdminDTO {
+    private int tableCode;
+    private boolean isReserved;
+    private Restaurant restaurant;
+}

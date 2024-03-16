827401c9241ec8a17fbe37a6cdd259e72d544e13
Asad Zaidi <73779880+technical-mindset@users.noreply.github.com>
2024-03-16 13:31:11 +0500

    Implemeting the generic pagination in base service

--- Backend Services/src/main/java/com/restaurant/backend/service/ItemCategoryService.java
+++ Backend Services/src/main/java/com/restaurant/backend/service/ItemCategoryService.java
@@ -18,6 +18,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
 import java.util.List;
 
+
 @Service
 @Transactional
 public class ItemCategoryService extends BaseService<ItemCategory, ItemCategoryDTO, ItemCategoryRepository>{
